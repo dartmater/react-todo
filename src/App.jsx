227e9f9126d1eda251fc88ef27c1fdf631045c15
Elle <rainbow@bk.ru>
2024-11-30 21:53:24 -0500

    revised lesson 1.3

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,13 @@ import AddTodoForm from "./AddTodoForm";
 function App() {
   // Define the todo list array
   const [newTodo, setNewTodo] = useState("");
+  const handleAddTodo = (todoTitle) => {
+    setNewTodo(todoTitle);
+  }
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={setNewTodo}/>
+      <AddTodoForm onAddTodo={handleAddTodo}/>
       <p>{newTodo}</p>
       <TodoList />
     </div>
